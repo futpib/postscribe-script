4601847deed027ba956cc82d8a818b1b6004aaaa
futpib <futpib@gmail.com>
2016-11-15 01:39:32 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+const postscribe = require('postscribe');
+
+const defaults = require('lodash.defaults');
+
+
+/**
+ * Add <script> tag to the end of the <body>, resolve after it executes
+ *
+ * @param {string} url Script to load (the 'src' attribute of the <script>)
+ * @param {object} options Subset of "postscribe" options (ecerything except callbacks is allowed)
+ * @returns {Promise} Promise that resolves after script has executed
+ */
+function postscribeScript (url, options) {
+  return new Promise(function (resolve, reject) {
+    const scriptElement = document.createElement('script');
+
+    scriptElement.setAttribute('type', 'text/javascript');
+    scriptElement.setAttribute('async', 'true');
+    scriptElement.setAttribute('src', url);
+
+    const scriptHTML = scriptElement.outerHTML;
+
+    postscribe(document.body, scriptHTML, defaults({
+      afterAsync: resolve,
+      error: reject,
+    }, options))
+  });
+}
+
+module.exports = postscribeScript;
